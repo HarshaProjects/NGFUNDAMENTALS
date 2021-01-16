307dcc23f08ca4145c9ee507250158457a7fa64f
harsha <harsha123@gmail.com>
2021-01-17 00:12:16 +0530

    Added Static resource like Images. Also added Styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: ` <h2>hello World</h2>
+    <img src="/assets/images/basic-shield.png" />`,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'NgFundamentals';
